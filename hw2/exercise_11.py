eec8bd57c5e76bc931a479a152fe1fe015510811
John Morgan <jtmorgan61@gmail.com>
2014-02-20 20:35:07 -0800

    completed unit 2

---
+++ hw2/exercise_11.py
@@ -0,0 +1,24 @@
+import datetime
+
+def reformat_subway_dates(convert_date):
+    '''
+    The dates in our subway data are formatted in the format month-day-year.
+    The dates in our weather underground data are formatted year-month-date.
+    
+    In order to join these two data sets together, we'll want the dates formatted
+    the same way.  Write a function that takes as its input a date in the MTA Subway
+    data format, and returns a date in the weather underground format.
+    
+    Hint: 
+    There is a useful function in the datetime library called strptime. 
+    More info can be seen here:
+    http://docs.python.org/2/library/datetime.html#datetime.datetime.strptime
+    '''
+
+    #takes string and returns datetime
+    date_formatted = datetime.datetime.strptime(date, "%m-%d-%y")
+    #takes datetime and returns string
+    date_formatted = datetime.datetime.strftime(date_formatted, "%Y-%m-%d")
+    return date_formatted
+
+#print reformat_subway_dates(today)
